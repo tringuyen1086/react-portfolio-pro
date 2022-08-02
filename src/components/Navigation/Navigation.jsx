ec71f66fe4ed581ee1af19a9715febfc1d0df4b6
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-08-02 13:12:41 -0700

    update Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -17,7 +17,6 @@ function Navigation() {
                         <ul>
                             <li><a href="https://github.com/tringuyen1086" target="_blank" rel="noreferrer"><GitHubIcon className="icon"></GitHubIcon></a></li>
                             <li><a href="https://www.linkedin.com/in/tri-nguyen-1086/" target="_blank" rel="noreferrer"><LinkedInIcon className="icon"></LinkedInIcon></a></li>
-                            <li><a href="https://twitter.com/TriNguy19394776" target="_blank" rel="noreferrer"><Twitter className="icon"></Twitter></a></li>
                         </ul>
                     </div>
                 </div>
