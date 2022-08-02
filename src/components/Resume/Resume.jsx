9f62eb418f94b8ae1c621cd6ba5ab7f1102b5608
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-08-02 13:09:05 -0700

    update resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -7,7 +7,7 @@ function Resume() {
     return (
         <div className="resume" id="resume">
             <h2>Tri Nguyen Resume</h2>
-            <p>Please check out <a href="https://drive.google.com/file/d/1Av95ptVgnmbibcUXxbhn0D39SjTHfVyU/view?usp=sharing" target="_blank" rel="noreferrer">downloadable resume here</a></p>
+            <p>Please check out <a href="https://drive.google.com/file/d/1zH2eC6FopspQwDVc7qZWNeL3EJ0f69z3/view?usp=sharing" target="_blank" rel="noreferrer">downloadable resume here</a></p>
             <h3>Full Stack Development Experience</h3>
             <p>Graduated from the UC Berkeley Extension Full Stack Development Coding Bootcamp, I have developed proficiency with these technologies:</p>
             <ul>
